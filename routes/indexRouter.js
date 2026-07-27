40629f60ef9b61a4e0a8d462bb8b8a08efa420bf
dmarinhoDKR <danielmarinho.b.f@gmail.com>
2026-07-27 16:40:02 -0300

    Stage 1: render in-memory messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,9 +2,23 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
 indexRouter.get("/", (_request, response) => {
     response.render("index", {
         title: "Mini Message Board",
+        messages,
     });
 });
 
